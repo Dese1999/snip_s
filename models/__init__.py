66588c1b53135e7602a9d5e4e7ae44343e285203
Dese1999 <mohadeseahmadii1378@gmail.com>
2025-08-18 10:19:52 +0330

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@
 
 # models/__init__.py
 from .split_googlenet import Split_googlenet
-from .split_resnet import Split_ResNet18, Split_ResNet34, Split_ResNet50, Split_ResNet101, Split_ResNet18Norm, Split_ResNet50Norm, Split_Xception
+from .split_resnet import Split_ResNet18, Split_ResNet34, Split_ResNet50, Split_ResNet101, Split_ResNet18Norm, Split_ResNet50Norm#, Split_Xception
 from .split_densenet import Split_densenet121, Split_densenet161, Split_densenet169, Split_densenet201
 from .split_vgg import vgg11, vgg11_bn
 
